76261319e18c7a94c1241878031067753545e745
Jørgen Grimnes <jorgenkg@gmail.com>
2015-01-10 14:42:13 +0100

    Minor: updated to support the changes in the net

--- backprop/main.py
+++ backprop/main.py
@@ -5,8 +5,8 @@ import numpy as np
 
 class Instance:
     def __init__(self, features, target):
-        self.features = np.matrix(features)
-        self.target = target
+        self.features = np.array(features)
+        self.targets = np.array(target)
 #end Instance
 
 
@@ -14,22 +14,20 @@ class Instance:
 training_one =  [ Instance( [0,0], [0] ), Instance( [0,1], [1] ), Instance( [1,0], [1] ), Instance( [1,1], [0] ) ]
 training_two =  [ Instance( [0,0], [0,0] ), Instance( [0,1], [1,1] ), Instance( [1,0], [1,1] ), Instance( [1,1], [0,0] ) ]
 
-
-
 n_inputs = 2
 n_outputs = 1
-n_hiddens = 6
+n_hiddens = 2
 n_hidden_layers = 1
 
 # specify activation functions per layer
-activation_functions = [ tanh_function ]*n_hidden_layers + [ linear_function ]
+activation_functions = [ tanh_function ]*n_hidden_layers + [ sigmoid_function ]
 
 # initialize your neural network
 network = NeuralNet(n_inputs, n_outputs, n_hiddens, n_hidden_layers, activation_functions)
 
 # start training
-network.backpropagation(training_one, ERROR_LIMIT=1e-3)
+network.backpropagation(training_one, ERROR_LIMIT=1e-4)
 
 
 for instance in training_one:
-    print instance.features, network.update( instance.features ), "\ttarget:", instance.target
+    print instance.features, network.update( np.array([instance.features]) ), "\ttarget:", instance.targets
