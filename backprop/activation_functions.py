db9501807de89c00f7ea8c15468d9eca408e43ce
Jørgen Grimnes <jorgenkg@gmail.com>
2015-04-30 10:53:27 +0200

    Added: symmetric elliot, Updated: ReLU
    The Symmetric Elliot function is a fast approximation to the `tanh`
    activation function
    
    The ReLU activation function has been modified to use the coarser, but
    faster, max(0,x) approximation

--- backprop/activation_functions.py
+++ backprop/activation_functions.py
@@ -17,16 +17,31 @@ def sigmoid_function( signal, derivative=False ):
         return signal
 #end activation function
 
-def ReLU_function( signal, derivative=False ):
-    # Prevent overflow.
-    signal = np.clip( signal, -500, 500 )
+def symmetric_elliot_function( signal, derivative=False ):
+    """
+    A fast approximation of tanh
+    """
     
+    s = 5 # steepness
+    
+    abs_signal = (1 + np.abs(signal * s))
     if derivative:
+        # Return the partial derivation of the activation function
+        return s / ( abs_signal * abs_signal )
+    else:
+        # Return the activation signal
+        return (signal * s) / abs_signal
+#end activation function
+
+def ReLU_function( signal, derivative=False ):
+    if derivative:
+        # Prevent overflow.
+        signal = np.clip( signal, -500, 500 )
         # Return the partial derivation of the activation function
         return expit( signal )
     else:
         # Return the activation signal
-        return np.log(1 + np.exp(signal))
+        return np.max(0, signal )
 #end activation function
 
 def tanh_function( signal, derivative=False ):
