eab7948a0932633605f6adb77f664e36832ee155
Jørgen Grimnes <jorgenkg@gmail.com>
2015-08-26 18:22:50 +0200

    Feature: added a saving and loading

--- backprop/neuralnet.py
+++ backprop/neuralnet.py
@@ -5,7 +5,7 @@ import itertools
 import collections
 
 class NeuralNet:
-    def __init__(self,n_inputs, n_outputs, n_hiddens, n_hidden_layers, activation_functions):
+    def __init__(self, n_inputs, n_outputs, n_hiddens, n_hidden_layers, activation_functions ):
         self.n_inputs = n_inputs                # Number of network input signals
         self.n_outputs = n_outputs              # Number of desired outputs from the network
         self.n_hiddens = n_hiddens              # Number of nodes in each hidden layer
@@ -69,7 +69,7 @@ class NeuralNet:
     def backpropagation(self, trainingset, ERROR_LIMIT=1e-3, learning_rate=0.3, momentum_factor=0.9  ):
         def addBias(A):
             # Add 1 as bias.
-            return np.hstack((np.ones((A.shape[0],1)),A))
+            return np.hstack(( np.ones((A.shape[0],1)), A ))
         #end addBias
         
         assert trainingset[0].features.shape[0] == self.n_inputs, "ERROR: input size varies from the defined input setting"
@@ -83,7 +83,7 @@ class NeuralNet:
         momentum = collections.defaultdict( int )
         
         epoch = 0
-        while MSE>ERROR_LIMIT:
+        while MSE > ERROR_LIMIT:
             epoch += 1
             
             input_layers      = self.update( training_data, trace=True )
@@ -93,7 +93,7 @@ class NeuralNet:
             delta             = error
             MSE               = np.mean( np.power(error,2) )
             
-            # Loop over the weight layers in reversed order to calculate the deltas
+            
             loop  = itertools.izip(
                             xrange(len(self.weights)-1, -1, -1),
                             reversed(self.weights),
@@ -102,8 +102,10 @@ class NeuralNet:
 
             
             for i, weight_layer, input_signals in loop:
+                # Loop over the weight layers in reversed order to calculate the deltas
+                
                 # Calculate weight change 
-                dW = learning_rate * np.dot( addBias(input_signals).T, delta )  + momentum_factor * momentum[i]
+                dW = learning_rate * np.dot( addBias(input_signals).T, delta ) + momentum_factor * momentum[i]
                 
                 if i!= 0:
                     """Do not calculate the delta unnecessarily."""
@@ -113,15 +115,17 @@ class NeuralNet:
                     # Calculate the delta for the subsequent layer
                     delta = np.multiply(  weight_delta, self.activation_functions[i-1]( input_signals, derivative=True) )
                 
-                # Store momentum
+                # Store the momentum
                 momentum[i] = dW
                 
-                # Update weights
+                # Update the weights
                 self.weights[ i ] += dW
             
-            if epoch%1000==0: print "* current network error (MSE):", MSE
+            if epoch%1000==0:
+                # Show the current training status
+                print "* current network error (MSE):", MSE
         
-        print "* Converged to error bound (%.3g) with MSE = %.3g." % ( ERROR_LIMIT, MSE )
+        print "* Converged to error bound (%.4g) with MSE = %.4g." % ( ERROR_LIMIT, MSE )
         print "* Trained for %d epochs." % epoch
     # end backprop
     
@@ -142,5 +146,48 @@ class NeuralNet:
         if trace: return tracelist
         
         return output
+    #end
+    
+    
+    def save_to_file(self, filename = "network.pkl" ):
+        import cPickle
+        """
+        This save method pickles the parameters of the current network into a 
+        binary file for persistant storage.
+        """
+        with open( filename , 'wb') as file:
+            store_dict = {
+                "n_inputs"             : self.n_inputs,
+                "n_outputs"            : self.n_outputs,
+                "n_hiddens"            : self.n_hiddens,
+                "n_hidden_layers"      : self.n_hidden_layers,
+                "activation_functions" : self.activation_functions,
+                "n_weights"            : self.n_weights,
+                "weights"              : self.weights
+
+            }
+            cPickle.dump( store_dict, file, 2 )
+    #end
+    
+    @staticmethod
+    def load_from_file( filename = "network.pkl" ):
+        """
+        Load the complete configuration of a previously stored network.
+        """
+        network = NeuralNet( 0, 0, 0, 0, [0] )
+        
+        with open( filename , 'rb') as file:
+            import cPickle
+            store_dict = cPickle.load(file)
+            
+            network.n_inputs             = store_dict["n_inputs"]            
+            network.n_outputs            = store_dict["n_outputs"]           
+            network.n_hiddens            = store_dict["n_hiddens"]           
+            network.n_hidden_layers      = store_dict["n_hidden_layers"]     
+            network.n_weights            = store_dict["n_weights"]           
+            network.weights              = store_dict["weights"]             
+            network.activation_functions = store_dict["activation_functions"]
+        
+        return network
     #end
 #end class
