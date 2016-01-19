b6c6b9934668f8c496c7443e48da8080294aa6b9
Jørgen Grimnes <jorgenkg@gmail.com>
2016-01-19 20:15:33 +0100

    Added an official wine benchmarking test

--- backprop/tools.py
+++ backprop/tools.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import os
 
 class Instance:
     # This is a simple encapsulation of a `input signal : output signal`
@@ -34,3 +34,39 @@ def confirm( promt='Do you want to continue?' ):
 			return False
 		print "Please enter y or n."
 #end
+
+def load_wine_data():
+    # UCI Machine Learning Repository http://archive.ics.uci.edu/ml/datasets/Wine
+    filename = "wine.data.txt"
+    
+    with open( filename, "r" ) as f:
+        entries = f.readlines()
+    
+    data = np.array([
+        entry[1:]
+        for entry in map(lambda x: map(float, x.split(",")), entries)
+    ])
+    
+    data /= np.max(data, axis=0) # naive value normalization
+    
+    assignments = [
+        int(entry[0]) # classification as integer
+        for entry in map(lambda x: map(float, x.split(",")), entries)
+    ]
+    
+    
+    targets = np.zeros( (data.shape[0], max(assignments)) )
+    
+    for i, classification in enumerate( assignments ):
+        # One-hot encoding
+        targets[i][classification-1] = 1
+    
+    
+    
+    return [
+        Instance( feature, target )
+        for feature, target in zip(data, targets)
+    ]
+#end
+    
+    
