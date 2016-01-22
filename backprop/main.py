bdca62e43a4c587db32d70fbcb42aa180cfef2d1
Jørgen Grimnes <jorgenkg@gmail.com>
2016-01-22 21:15:26 +0100

    updated to reflect the removal of the wine dataset

--- backprop/main.py
+++ backprop/main.py
@@ -1,20 +1,17 @@
 from activation_functions import sigmoid_function, tanh_function, linear_function,\
                                  LReLU_function, ReLU_function, elliot_function, symmetric_elliot_function
 from neuralnet import NeuralNet
-from tools import Instance, load_wine_data
+from tools import Instance
 import numpy as np
 
 
 # Training sets
 training_one    = [ Instance( [0,0], [0] ), Instance( [0,1], [1] ), Instance( [1,0], [1] ), Instance( [1,1], [0] ) ]
 
-training_wine   = load_wine_data() # Loads the dataset from UCI Machine Learning Repository http://archive.ics.uci.edu/ml/datasets/Wine
-
-
 settings = {
     # Required settings
-    "n_inputs"              : 13,        # Number of network input signals
-    "layers"                : [ (3, tanh_function), (3, sigmoid_function) ],
+    "n_inputs"              : 2,       # Number of network input signals
+    "layers"                : [ (3, tanh_function), (1, sigmoid_function) ],
                                         # [ (number_of_neurons, activation_function) ]
                                         # The last pair in you list describes the number of output signals
     
@@ -23,8 +20,8 @@ settings = {
     "weights_high"          : 0.1,      # Upper bound on initial weight range
     "save_trained_network"  : False,    # Whether to write the trained weights to disk
     
-    "input_layer_dropout"   : 0.2,      # dropout fraction of the input layer
-    "hidden_layer_dropout"  : 0.5,      # dropout fraction in all hidden layers
+    "input_layer_dropout"   : 0.0,      # dropout fraction of the input layer
+    "hidden_layer_dropout"  : 0.0,      # dropout fraction in all hidden layers
 }
 
 
@@ -34,14 +31,35 @@ network = NeuralNet( settings )
 # load a stored network configuration
 # network = NeuralNet.load_from_file( "trained_configuration.pkl" )
 
-
-# start training on test set one
-network.backpropagation( 
-                training_wine,           # specify the training set
-                ERROR_LIMIT     = 1e-3,  # define an acceptable error limit 
-                learning_rate   = 0.03,  # learning rate
-                momentum_factor = 0.45,   # momentum
-                #max_iterations  = 100,  # continues until the error limit is reach if this argument is skipped
+# Train the network using Scaled Conjugate Gradient
+network.scg(
+                training_one, 
+                ERROR_LIMIT = 1e-4
             )
 
-print "Final MSE:", network.test( training_wine )
+# Train the network using backpropagation
+#network.backpropagation( 
+#                training_one,          # specify the training set
+#                ERROR_LIMIT     = 1e-3, # define an acceptable error limit 
+#                #max_iterations  = 100, # continues until the error limit is reach if this argument is skipped
+#
+#                # optional parameters
+#                learning_rate   = 0.03, # learning rate
+#                momentum_factor = 0.45, # momentum
+#            )
+
+# Train the network using resilient backpropagation
+#network.resilient_backpropagation( 
+#                training_one,          # specify the training set
+#                ERROR_LIMIT     = 1e-3, # define an acceptable error limit
+#                #max_iterations = (),   # continues until the error limit is reach if this argument is skipped
+#                
+#                # optional parameters
+#                weight_step_max = 50., 
+#                weight_step_min = 0., 
+#                start_step = 0.5, 
+#                learn_max = 1.2, 
+#                learn_min = 0.5
+#            )
+
+print "Final MSE:", network.test( training_one )
