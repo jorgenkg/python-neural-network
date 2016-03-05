76013260cbaec19e1854ea7ec81ba0e8ab25616a
Jørgen Grimnes <jorgenkg@gmail.com>
2016-03-05 10:41:01 +0100

    dirty fix: the cross entropy function is buggy
    the cross entropy function is buggy and shouldn't have been committed

--- backprop/cost_functions.py
+++ backprop/cost_functions.py
@@ -8,13 +8,13 @@ def sum_squared_error( outputs, targets, derivative=False ):
         return np.sum( np.power(outputs - targets,2) )
 #end cost function
 
-
-def cross_entropy_cost( outputs, targets, derivative=False ):
-    if derivative:
-        return (outputs - targets) / (outputs * (1 - outputs)) 
-    else:
-        return sum(
-            -np.inner(target, output) - np.inner((1 - target), np.log(1 - output ))
-            for output, target in zip(outputs, targets)
-        ) / float(outputs.shape[0])
-#end cost function
+# buggy
+#def cross_entropy_cost( outputs, targets, derivative=False ):
+#    if derivative:
+#        return (outputs - targets) / (outputs * (1 - outputs)) 
+#    else:
+#        return sum(
+#            -np.inner(target, output) - np.inner((1 - target), np.log(1 - output ))
+#            for output, target in zip(outputs, targets)
+#        ) / float(outputs.shape[0])
+##end cost function
