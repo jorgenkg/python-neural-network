5ae48c1ea1fa4bc52eab98f3884a19c99a247008
Jørgen Grimnes <jorgenkg@gmail.com>
2016-04-16 16:26:00 +0200

    moved the save() function into the neuralnet class

--- backprop/learning_algorithms.py
+++ backprop/learning_algorithms.py
@@ -1,4 +1,4 @@
-from tools import dropout, add_bias, confirm, save_network_to_file
+from tools import dropout, add_bias, confirm
 from activation_functions import softmax_function
 from cost_functions import softmax_neg_loss
 import numpy as np
@@ -83,7 +83,7 @@ def backpropagation(network, trainingset, testset, cost_function, ERROR_LIMIT = 
     print "[training]   Trained for %d epochs." % epoch
     
     if save_trained_network and confirm( promt = "Do you wish to store the trained network?" ):
-        save_network_to_file( network )
+        network.save_network_to_file()
 # end backprop
 
 
@@ -201,7 +201,7 @@ def resilient_backpropagation(network, trainingset, testset, cost_function, ERRO
     print "[training]   Trained for %d epochs." % epoch
     
     if save_trained_network and confirm( promt = "Do you wish to store the trained network?" ):
-        save_network_to_file( network )
+        network.save_network_to_file()
 # end backprop
 
 
@@ -245,7 +245,7 @@ def scipyoptimize(network, trainingset, testset, cost_function, method = "Newton
         print "[training]   Completed with error %.4g." % results.fun
         
         if save_trained_network and confirm( promt = "Do you wish to store the trained network?" ):
-            save_network_to_file( network )
+            network.save_network_to_file()
 #end
 
 
@@ -353,60 +353,61 @@ def scaled_conjugate_gradient(network, trainingset, testset, cost_function, ERRO
     
     
     if save_trained_network and confirm( promt = "Do you wish to store the trained network?" ):
-        save_network_to_file( network )
+        network.save_network_to_file()
 #end scg
 
 
-def generalized_hebbian(network, trainingset, cost_function, ERROR_LIMIT = 1e-3, learning_rate = 0.001, max_iterations = (), save_trained_network = False ):
-    assert softmax_function != network.layers[-1][1] or cost_function == softmax_neg_loss,\
-        "When using the `softmax` activation function, the cost function MUST be `softmax_neg_loss`."
-    assert cost_function != softmax_neg_loss or softmax_function == network.layers[-1][1],\
-        "When using the `softmax_neg_loss` cost function, the activation function in the final layer MUST be `softmax`."
-        
-    assert trainingset[0].features.shape[0] == network.n_inputs, \
-        "ERROR: input size varies from the defined input setting"
-    assert trainingset[0].targets.shape[0]  == network.layers[-1][0], \
-        "ERROR: output size varies from the defined output setting"
-    
-    training_data              = np.array( [instance.features for instance in trainingset ] )
-    training_targets           = np.array( [instance.targets  for instance in trainingset ] )
-    test_data                  = np.array( [instance.features  for instance in testset ] )
-    test_targets               = np.array( [instance.targets  for instance in testset ] )
-                                
-    layer_indexes               = range( len(network.layers) )
-    epoch                       = 0
-                                
-    input_signals, derivatives  = network.update( training_data, trace=True )
-                                
-    out                         = input_signals[-1]
-    error                       = cost_function( out, training_targets )
-    
-    input_signals[-1] = out - training_targets
-    
-    while error > ERROR_LIMIT and epoch < max_iterations:
-        epoch += 1
-        
-        for i in layer_indexes:
-            forgetting_term     = np.dot(network.weights[i], np.tril(np.dot( input_signals[i+1].T, input_signals[i+1] )))
-            activation_product  = np.dot(add_bias(input_signals[i]).T, input_signals[i+1])
-            network.weights[i] += learning_rate * (activation_product - forgetting_term)
-        #end weight adjustment loop
-        
-        # normalize the weight to prevent the weights from growing unbounded
-        network.weights[i]     /= np.sqrt(np.sum(network.weights[i]**2))
-        
-        input_signals, derivatives = network.update( training_data, trace=True )
-        out                        = input_signals[-1]
-        error                      = cost_function(out, training_targets )
-        
-        if epoch%1000==0:
-            # Show the current training status
-            print "[training] Current error:", error, "\tEpoch:", epoch
-    
-    print "[training] Finished:"
-    print "[training]   Converged to error bound (%.4g) with error %.4g." % ( ERROR_LIMIT, error )
-    print "[training]   Trained for %d epochs." % epoch
-    
-    if save_trained_network and confirm( promt = "Do you wish to store the trained network?" ):
-        save_network_to_file( network )
-# end backprop
+# NOT YET IMPLEMENTED
+#def generalized_hebbian(network, trainingset, testset, cost_function, ERROR_LIMIT = 1e-3, learning_rate = 0.001, max_iterations = (), save_trained_network = False ):
+#    assert softmax_function != network.layers[-1][1] or cost_function == softmax_neg_loss,\
+#        "When using the `softmax` activation function, the cost function MUST be `softmax_neg_loss`."
+#    assert cost_function != softmax_neg_loss or softmax_function == network.layers[-1][1],\
+#        "When using the `softmax_neg_loss` cost function, the activation function in the final layer MUST be `softmax`."
+#        
+#    assert trainingset[0].features.shape[0] == network.n_inputs, \
+#        "ERROR: input size varies from the defined input setting"
+#    assert trainingset[0].targets.shape[0]  == network.layers[-1][0], \
+#        "ERROR: output size varies from the defined output setting"
+#    
+#    training_data              = np.array( [instance.features for instance in trainingset ] )
+#    training_targets           = np.array( [instance.targets  for instance in trainingset ] )
+#    test_data                  = np.array( [instance.features  for instance in testset ] )
+#    test_targets               = np.array( [instance.targets  for instance in testset ] )
+#                                
+#    layer_indexes               = range( len(network.layers) )
+#    epoch                       = 0
+#                                
+#    input_signals, derivatives  = network.update( training_data, trace=True )
+#                                
+#    out                         = input_signals[-1]
+#    error                       = cost_function( out, training_targets )
+#    
+#    input_signals[-1] = out - training_targets
+#    
+#    while error > ERROR_LIMIT and epoch < max_iterations:
+#        epoch += 1
+#        
+#        for i in layer_indexes:
+#            forgetting_term     = np.dot(network.weights[i], np.tril(np.dot( input_signals[i+1].T, input_signals[i+1] )))
+#            activation_product  = np.dot(add_bias(input_signals[i]).T, input_signals[i+1])
+#            network.weights[i] += learning_rate * (activation_product - forgetting_term)
+#        #end weight adjustment loop
+#        
+#        # normalize the weight to prevent the weights from growing unbounded
+#        network.weights[i]     /= np.sqrt(np.sum(network.weights[i]**2))
+#        
+#        input_signals, derivatives = network.update( training_data, trace=True )
+#        out                        = input_signals[-1]
+#        error                      = cost_function(out, training_targets )
+#        
+#        if epoch%1000==0:
+#            # Show the current training status
+#            print "[training] Current error:", error, "\tEpoch:", epoch
+#    
+#    print "[training] Finished:"
+#    print "[training]   Converged to error bound (%.4g) with error %.4g." % ( ERROR_LIMIT, error )
+#    print "[training]   Trained for %d epochs." % epoch
+#    
+#    if save_trained_network and confirm( promt = "Do you wish to store the trained network?" ):
+#        network.save_network_to_file()
+## end backprop
