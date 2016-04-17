14b40a92cbc89f3ba909568c7489a628b10d761c
Jørgen Grimnes <jorgenkg@gmail.com>
2016-04-17 14:36:41 +0200

    Removed unused code: Dataset.class

--- backprop/data_structures.py
+++ backprop/data_structures.py
@@ -1,30 +1,15 @@
 from preprocessing import *
+import numpy as np
 
 class Instance:
     # This is a simple encapsulation of a `input signal : output signal`
     # pair in our training set.
-    def __init__(self, features, target):
+    def __init__(self, features, target = None ):
         self.features = np.array(features)
-        self.targets  = np.array(target)
+        
+        if target:
+            self.targets  = np.array(target)
+        else:
+            self.targets  = None
 #endclass Instance
 
-
-class Dataset:
-    def __init__(self, instance_list ):
-        self.features = np.array( [instance.features for instance in instance_list ] )
-        self.targets = np.array( [instance.targets for instance in instance_list ] )
-    #end
-    
-    def preprocess(self, ):
-        self._features = self.features
-        
-        decorrelate  = preprocessing_decorrelation( self.features )
-        standardize  = preprocessing_standarize( decorrelate(self.features) )
-        
-        encoder = lambda matrix: standardize( decorrelate( matrix ))
-        self.features = encoder( self.features )
-        
-        return encoder
-    #end
-#endclass Dataset
-    
