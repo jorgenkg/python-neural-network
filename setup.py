0fdde6e09926c19153a8126b2f52e9cf14768a23
Jørgen Grimnes <jorgenkg@gmail.com>
2016-08-13 18:39:30 +0200

    updated

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ except ImportError:
 setup(
   name             = 'nimblenet',
   packages         = find_packages(),
-  version          = '0.2',
+  version          = '0.2.1',
   description      = 'Efficient python (NumPy) neural network library.',
   long_description = 'This is an efficient implementation of a fully connected neural network in NumPy. The network can be trained by a variety of learning algorithms: backpropagation, resilient backpropagation and scaled conjugate gradient learning. The network has been developed with PYPY in mind.',
   author           = 'Jorgen Grimnes',
   author_email     = 'jorgenkg@yahoo.no',
   url              = 'https://jorgenkg.github.io/python-neural-network/',
-  download_url     = 'https://github.com/jorgenkg/python-neural-network/tarball/0.2',
+  download_url     = 'https://github.com/jorgenkg/python-neural-network/tarball/0.2.1',
   keywords         = ["python", "numpy", "neuralnetwork", "neural", "network", "efficient", "lightweight"],
   install_requires = [ 'numpy' ],
   extras_require   = {
